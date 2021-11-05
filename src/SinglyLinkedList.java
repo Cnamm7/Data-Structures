b8b4b854d4a3dcfdaebf43eb825e18e3453779fe
Cnamm7 <Cna.mm7@gmail.com>
2021-11-04 21:16:29 -0400

    writing singly Linked list

---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,46 @@
+
+public class SinglyLinkedList {
+	public class Token {
+		String data;
+		Token next;
+		Token(String inputS)
+		{
+			data = inputS;
+			next = null;
+		}
+	}
+	int count = 0;
+	Token head = new Token(null);
+	Boolean isEmpty()
+	{
+		if(count == 0) {return true;}
+		else {return false;}
+	}	
+	void insertAtEnd(String input)
+	{
+		Token newToken = new Token(input);
+		if(this.isEmpty())
+		{
+			head.next = newToken;
+		}else {
+			Token temp = head.next;
+			while(temp.next != null)
+			{
+				temp = temp.next;
+			}
+			temp.next = newToken;	
+		}
+		count++;	
+	}
+	void print()
+	{
+		Token temp = head.next;
+		System.out.print("Singly Linked List = [");
+		while(temp.next != null)
+		{
+			System.out.print(temp.data + ", ");
+			temp = temp.next;			
+		}
+		System.out.println(temp.data + "]");
+	}	
+}
--- src/main.java
+++ src/main.java
@@ -1,9 +1,16 @@
-
-public class main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+public class Main {
+  public static void main(String[] args) {
+	  SinglyLinkedList l = new SinglyLinkedList();
+	  boolean m = l.isEmpty();
+	  System.out.println(m);
+	  l.insertAtEnd("sina");
+	  l.insertAtEnd("baba");
+	  l.insertAtEnd("saba");
+	  boolean m1 = l.isEmpty();
+	  System.out.println(m1);
+	  l.print();
+	  System.out.println("the list contains " + l.count + " elements.");
+  }
 
 }
+
